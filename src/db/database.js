02fd7aba1e2f67238fd86123fdcaba96ddc278f4
rickph <32214732+rickph@users.noreply.github.com>
2026-04-29 17:18:30 +0800

    Update database.js

--- src/db/database.js
+++ src/db/database.js
@@ -20,7 +20,7 @@ function getPool() {
   }
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+    ssl: { rejectUnauthorized: false },
     max: 10,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 5000,
